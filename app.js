fd6320e09f77a089465fc062136527c756cffac6
caleosun123 <osunagarciacaleb@gmail.com>
2025-01-31 12:16:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, 'views'));
