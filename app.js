7d72d415cb2aea25b24d28c5093caf89421c430d
caleosun123 <osunagarciacaleb@gmail.com>
2025-01-31 12:08:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Hello, world!" });
 });
 
+app.post("/submit", (req, res) -> {
+  const message = req.body.message;
+  res.send(`You said: ${message}`);
+});
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
